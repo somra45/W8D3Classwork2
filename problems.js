8c4ca9998d08efc7b08246a9fde6cb76c2d6bc47
root <somra45@gmail.com>
2023-06-22 00:38:18 -0400

    W8D3 classwork

---
+++ problems.js
@@ -0,0 +1,150 @@
+class Clock {
+    constructor() {
+        const currentTime = new Date()
+        this.hours = currentTime.getUTCHours();
+        this.minutes = currentTime.getUTCMinutes();
+        this.seconds = currentTime.getUTCSeconds();
+        
+        console.log(this.printTime());
+        
+        setInterval(this._tick.bind(this), 1000);
+    };
+
+    printTime() {
+        let time = `${this.hours}:${this.minutes}:${this.seconds}`
+        console.log(`${time}`);
+    };
+
+    _tick() {
+        this.seconds++
+        if (this.seconds === 60) {
+            this.seconds = 0
+            this.minutes++
+        }
+        if (this.minutes === 60) {
+            this.minutes = 0
+            this.hours++
+        }
+        if (this.hours === 24) {
+            this.hours = 0
+        }
+        this.printTime();
+    };
+};
+
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const addNumbers = function(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question(`Please enter a number`, (res) => {
+            let parsed = parseInt(res);
+            sum += parsed;
+            console.log(`The sum is now ${sum}.`);
+            addNumbers(sum, --numsLeft, completionCallback);
+            if (numsLeft === 0) {
+                completionCallback(sum);
+                reader.close()
+            }
+        });
+    }
+
+    // const completionCallback = function() {
+    //     console.log(`No numbers left, your final sum is ${sum}`)
+    // }
+};
+
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+
+// Function.prototype.myBind = function(context) {
+//     let that = this
+//     let arrow = function(){
+//         that.apply(context)
+//     }
+//     return arrow
+// }
+
+// class Lamp {
+//     constructor() {
+//       this.name = "a lamp";
+//     }
+//   }
+  
+//   const turnOn = function() {
+//     console.log("Turning on " + this.name);
+//   };
+  
+//   const lamp = new Lamp();
+  
+//   turnOn(); // should not work the way we want it to
+  
+//   const boundTurnOn = turnOn.bind(lamp);
+//   const myBoundTurnOn = turnOn.myBind(lamp);
+  
+//   boundTurnOn(); // should say "Turning on a lamp"
+//   myBoundTurnOn(); // should say "Turning on a lamp"
+
+const reader1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function askIfGreaterThan(el1, el2, callback) {
+    if (el1 > el2){
+    reader.question(`Is ${el1} greater than ${el2}?(type yes or no)`, response => {
+        if (response === 'yes') {
+            callback(true)
+        }
+        else {
+            callback(false)
+        };
+    })};
+};
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+    
+    if (i === arr.length - 1) {
+        outerBubbleSortLoop(madeAnySwaps)
+    }
+    else if (i < arr.length - 1) {
+        askIfGreaterThan(arr[i], arr[i+1], function(isGreaterThan) {
+            if (isGreaterThan === true) {
+                let first = arr[i]
+                let second = arr[i+1]
+                arr[i] = second
+                arr[i+1] = first
+                madeAnySwaps = true
+            };
+        });
+        innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop)
+    };
+};
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+    function outerBubbleSortLoop(madeAnySwaps) {
+        if (madeAnySwaps === true) {
+            innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop)
+        }
+        else { 
+            sortCompletionCallback(arr)
+        };
+    };
+    outerBubbleSortLoop(true)
+  };
+
+// let test = function(bool){
+//     console.log(bool)
+// }
+
+// askIfGreaterThan(10, 5, test)
+
+absurdBubbleSort([3, 2, 1], function(arr) {
+    console.log("Sorted array: " + JSON.stringify(arr));
+    reader.close();
+  });
---
+++ tic-tac-toe-js/board.js
@@ -0,0 +1,17 @@
+class Board {
+  won() {
+
+  };
+
+  winner() {
+    if (hasWon === true) {
+    console.log(`You are the winner!`)
+  }};
+
+  empty(pos) {
+    if (pos.haspiece === true) {
+
+    } return false; 
+  }
+      
+};
